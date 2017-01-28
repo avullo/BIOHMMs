7ed633c7f1a7d641701ed75dff92946178175ba4
Alessandro Vullo <avullo@ebi.ac.uk>
2017-01-28 18:41:43 +0000

    Explanations of various class attributes.

--- BIOHMM.h
+++ BIOHMM.h
@@ -1,12 +1,15 @@
+/*
+
+  A class to represent bidirectional IOHMMs
+
+ */
+
 #ifndef BIOHMM_H
 #define BIOHMM_H
 
 #include <cstdlib>
 #include <cmath>
 #include <iostream>
-#include "General.h"
-//#include "NN.h"
-//#include "NNt.h"
 
 #define MAX 2500
 
@@ -14,26 +17,46 @@
 // BIOHMM ver. 1.0 (16/9/2005)
 // Copyright (C) Gianluca Pollastri 2005
 
-
-
-
 class BIOHMM {
- private:
+  /*
+    Input, output and forward/backward states are multinomial random variables
+    Store the number of values for each of them.
+   */
+  int NI; // number of values of I_t (random variable representing input at time t)
+  int NO; // the same for O_t (output at time t)
+  int NF; // number of realisations of F_t (forward state variable at time t)
+  int NB; // the same for B_t (backward state variable at t)
 
-  int NI;
-  int NO;
-  int NF;
-  int NB;
+  /*
+    Model parameters
 
-//  NN* NetOut;
-//  NNt* NetF;
-//  NNt* NetB;
+    The parameters of a BN specify the local conditional distribution of each
+    variable given its parents. In this case, the distributions are:
 
-  Float* mua;
-  Float* mub;
-  Float* mu1;
+    P(O_t|F_t, B_t, I_t)
+    P(F_t|F_t-1, I_t)
+    P(B_t|B_t+1, I_t)
 
-  // junction tree cluster potentials
+    In the discrete case, these can be explicitly represented using conditional
+    propability tables (CPTs). 
+    The assumption here is that the model is stationary, i.e. the above CPTs do
+    not vary over time, which can be seen as a form of parameter sharing which
+    significantly reduces the degrees of freedom.
+    
+    NOTE: the unconditional distribution of root nodes (i.e. P(U_t)) do not need 
+    to be modeled since we assume there are no missing data in the input sequence.
+  */
+  Float**** P_OFBI;
+  Float*** P_FFI;
+  Float*** P_BBI;
+
+  // To be complete, the model needs to specify the distributions of the boundary
+  // varialbles, i.e. P(F_0), P(B_T+1)
+  Float* P_F;
+  Float* P_B;
+
+  // Junction tree cluster (i.e. clique) potentials
+  // Cliques are (I_t, F_t, B_t, Y_t), (I_t, B_t, F_t, F_t-1), (I_t, F_t, B_t, B_T+1)
   Float***** OFBI;
   Float***** FFBI;
   Float***** BBFI;
@@ -43,18 +66,24 @@ class BIOHMM {
   Float**** FBIb;
   Float*** FB1;
 
-  // CPTs
-  Float**** P_OFBI;
-  Float*** P_FFI;
-  Float*** P_BBI;
+  Float* mua;
+  Float* mub;
+  Float* mu1;
 
-  // CPT sufficient stats?
+  /*
+    Learning is formulated in the context of maximum likelihood (ML) and is
+    solved by the expectation maximisation (EM) algorithm.
+
+    In the case of belief networks, the E-step consists of computing the 
+    expected sufficient statistics for the parameters. In the case of multinomial
+    tables, the M-step consists in updating each entry of a table by the
+    corresponding normalised expected counts.
+    
+    These tables store the expected counts.
+   */
   Float**** P_OFBIss;
   Float*** P_FFIss;
   Float*** P_BBIss;
-
-  Float* P_F;
-  Float* P_B;
 
   Float* Y;
 
@@ -85,8 +114,6 @@ class BIOHMM {
 
   void saveOutput(int length);
 
-//  int max(Float*a,int m);
-
  public:
 
   BIOHMM(int NI, int NO, int NF, int NB);
@@ -96,30 +123,16 @@ class BIOHMM {
   void propagate(int length);
   void sufficientStats(int length);
 
-//  void extimation(Float* seq, int* y, int length, int backp=0);
   void extimation(int* seq, int* y, int length);
-
-//  void extimation(Float* seq, Float* y, int length, int backp=0);
-//  void extimation(int* seq, Float* y, int length, int backp=0);
-
   void maximization(Float att=0.1, Float prior=0.05);
 
-//  void Feed(Float* seq, int length);
   void Feed(int* seq, int length);
-//  void predict(Float* seq, int length);
   void predict(int* seq, int length);
 
-
-  Float* out() {return Y;}
-
-  Float getError() {
-	return error;
-  };
-  void resetError() {
-	error=0.0;
-  };
-
-
+  Float* out() { return Y; }
+  Float getError() { return error; }
+  void resetError() { error =.0; }
+  
 };
 
 
